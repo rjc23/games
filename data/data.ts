e4dce34fdeab325a66bb7346b15cc4186428bb84
Ryan J Carmody <ryancarmody@outlook.com>
2022-06-27 18:05:33 +0930

    adding vercel config

--- data/data.ts
+++ data/data.ts
@@ -14,19 +14,5 @@ export const games: Game[] = [
         img: "world.png",
         desc: "The game where you get six guesses to try and guess which country it is. Choose from 3 game modes.",
         link: "https://playworldle.com"
-    },
-    {
-        id: 3,
-        title: "Snake",
-        img: "snake.jpeg",
-        desc: "Play the classic mobile Snake game for free online. Eat the food but don’t hit the walls or your own body!",
-        link: "https://playretrosnake.com"
-    },
-    {
-        id: 4,
-        title: "Worldle",
-        img: "world.png",
-        desc: "The game where you get six guesses to try and guess which country it is. Choose from 3 game modes.",
-        link: "https://playworldle.com"
     }
 ]
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'games-two',
+    title: 'Games by Ryan Carmody',
     htmlAttrs: {
       lang: 'en'
     },
