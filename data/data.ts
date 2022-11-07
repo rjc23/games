83bba4e34f81988388e81166cfac3f89805c8990
Ryan J Carmody <ryancarmody@outlook.com>
2022-11-08 00:26:26 +1030

    wheretkaen

--- data/data.ts
+++ data/data.ts
@@ -3,13 +3,20 @@ import { Game } from "~/interfaces/interfaces";
 export const games: Game[] = [
     {
         id: 1,
+        title: "WhereTaken",
+        img: "wheretaken.png",
+        desc: "Guess the country from a photo. New game daily.",
+        link: "https://wheretaken.com"
+    },
+    {
+        id: 2,
         title: "Snake",
         img: "snake.jpeg",
         desc: "Play the classic mobile Snake game for free online. Eat the food but don’t hit the walls or your own body!",
         link: "https://playretrosnake.com"
     },
     {
-        id: 2,
+        id: 3,
         title: "Worldle",
         img: "world.png",
         desc: "The game where you get six guesses to try and guess which country it is. Choose from 3 game modes.",
