583d555b2607d5437708186dbd29e20cbdfdae81
Ryan Carmody <ryan@Ryans-MacBook-Pro.local>
2023-01-19 15:15:47 +1030

    update games to add smug chickebn

--- data/data.ts
+++ data/data.ts
@@ -3,23 +3,23 @@ import { Game } from "~/interfaces/interfaces";
 export const games: Game[] = [
     {
         id: 1,
+        title: "Smug Chicken",
+        img: "smug-full.png",
+        desc: "The daily AI art word game.",
+        link: "https://smugchicken.com"
+    },
+    {
+        id: 2,
         title: "WhereTaken",
         img: "wheretaken.png",
         desc: "Guess the country or region from a photo. New game daily.",
         link: "https://wheretaken.com"
     },
     {
-        id: 2,
+        id: 3,
         title: "Snake",
         img: "snake.jpeg",
-        desc: "Play the classic mobile Snake game for free online. Eat the food but don’t hit the walls or your own body!",
+        desc: "Play the classic mobile Snake game for free online.",
         link: "https://playretrosnake.com"
-    },
-    {
-        id: 3,
-        title: "Worldle",
-        img: "world.png",
-        desc: "The game where you get six guesses to try and guess which country it is. Choose from 3 game modes.",
-        link: "https://playworldle.com"
     }
 ]
